d96b9ccd4e1d0a37742f3837e9f63c941c80fc42
Lucas Somacal <lsomacal@gmail.com>
2024-01-25 20:25:32 -0300

    Diálogo de guardado de nueva muestra

--- GUI/NuevaMuestra.py
+++ GUI/NuevaMuestra.py
@@ -1,7 +1,9 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+
 from GUI.nueva_muestra_ui import Ui_NuevaMuestraWindow
 from Muestra import Muestra
-from utils import save_pickle
+from utils import guardar_muestra
 
 
 class NuevaMuestraWindow(QtWidgets.QMainWindow, Ui_NuevaMuestraWindow):
@@ -17,6 +19,16 @@ class NuevaMuestraWindow(QtWidgets.QMainWindow, Ui_NuevaMuestraWindow):
         self.cancelar_aceptar_boton.accepted.connect(self.aceptar)
         self.cancelar_aceptar_boton.rejected.connect(self.cancelar)
 
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self, "Guardar muestra", "../muestras",
+                                                  "All Files (*);;Muestras (*.mtra)", options=options)
+        if fileName:
+            print(fileName)
+            return fileName
+        return None
+
     def aceptar(self):
         nueva_muestra = Muestra(self.nombre.text(),
                                 self.fecha.date().toPyDate(),
@@ -26,7 +38,10 @@ class NuevaMuestraWindow(QtWidgets.QMainWindow, Ui_NuevaMuestraWindow):
                                 self.cantidad_lecturas.value(),
                                 self.observaciones.toPlainText(),
                                 self.mapa)
-        save_pickle(nueva_muestra, "muestra_prueba.pkl", verbose=True)
+        fileName = self.saveFileDialog()
+        if fileName is not None:
+            guardar_muestra(nueva_muestra, fileName, verbose=True)
+        self.close()
 
     def cancelar(self):
         self.close()
--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ def file_extension(p):
     return os.path.splitext(p)[1]
 
 
-def save_pickle(obj, file_name: str, verbose=False):
-    if file_extension(file_name) != '.pkl':
-        file_name += '.pkl'
+def guardar_muestra(obj, file_name: str, verbose=False):
+    if file_extension(file_name) != '.mtra':
+        file_name += '.mtra'
 
     directory = os.path.dirname(file_name)
     if not os.path.isdir(directory) and directory != '':
@@ -21,8 +21,8 @@ def save_pickle(obj, file_name: str, verbose=False):
 
 
 def load_pickle(file_name: str, verbose=False):
-    if file_extension(file_name) != '.pkl':
-        file_name += '.pkl'
+    if file_extension(file_name) != '.mtra':
+        file_name += '.mtra'
 
     with open(file_name, 'rb') as f:
         p = pickle.load(f)
