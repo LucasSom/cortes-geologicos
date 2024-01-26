181db333e32c22b2a87d4a3b5b9ffa30dd7eb4a1
Lucas Somacal <lsomacal@gmail.com>
2024-01-26 19:23:47 -0300

    main.py llama a menu_principal.py

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import sys
+from PyQt5 import QtWidgets
 
-from PyQt5.QtGui import QFont, QIcon
-from PyQt5.QtWidgets import QApplication
-
-
-def main_menu(argv):
-    app = QApplication(argv)
-    app.setStyle('Plastic style')
-    # app.setStyleSheet('')
-    # app.setFont(QFont(''))
-    # app.setWindowIcon(QIcon(''))
-    # menu = MainMenu()
-
-    sys.exit(app.exec_())
-
+from GUI.menu_principal import MainWindow
 
 if __name__ == '__main__':
-    main_menu(sys.argv)
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
 
