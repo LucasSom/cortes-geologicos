5e8d8943c006cd1c1861164d808b23ed77361a80
Lucas Somacal <lsomacal@gmail.com>
2024-08-24 17:56:35 -0700

    Versionado

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import requests
 from PyQt5 import QtWidgets
 
 from GUI.menu_principal import MainWindow
+from utils import info_window
+
+
+def check_for_update(current_version, w):
+    response = requests.get("https://api.github.com/repos/LucasSom/cortes-geologicos/releases/latest")
+    latest_version = response.json()["tag_name"]
+
+    if current_version < latest_version:
+        notify_user(latest_version, w)
+
+
+def notify_user(latest_version, w):
+    info_window(w, f"Hay una nueva versión disponible: {latest_version}.\n"
+                   f"Actualizar descargando de https://github.com/LucasSom/cortes-geologicos")
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     window = MainWindow()
+    check_for_update("v1.0.7", window)
     window.show()
     app.exec_()
-
